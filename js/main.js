cdcfa7ecceaced0bb3afa369a1d8eec544c0531b
janinewong <jaeninwong@gmail.com>
2015-11-15 19:19:12 -0800

    Added new portfolio home page

---
+++ js/main.js
@@ -0,0 +1,40 @@
+function init() {
+ window.addEventListener('scroll', function(e){
+    var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+      shrinkOn = 100,
+      mobileShrinkOn = 10,
+      mq = window.matchMedia( "(max-width: 65em)" );
+
+    if (mq.matches) {
+      if(distanceY > mobileShrinkOn) {
+        $('nav img').addClass('small-logo');
+        $('nav img').removeClass('logo');
+      }
+      
+       if(distanceY < mobileShrinkOn) {
+        $('nav img').addClass('logo');
+        $('nav img').removeClass('small-logo');
+      }
+    } 
+
+    else {
+      if(distanceY > shrinkOn) {
+        $('nav img').addClass('small-logo');
+        $('nav img').removeClass('logo');
+        $('.small-logo').css({'padding-bottom':'1em'});
+
+        $('.logo-title').css({'display':'none'});
+      }
+
+      if(distanceY < shrinkOn) {
+        $('nav img').addClass('logo');
+        $('nav img').removeClass('small-logo');
+        $('.logo').css({'padding-bottom':'1.5em'});
+
+        $('.logo-title').css({'display':'block'});
+      }
+    }
+  });
+}
+
+window.onload = init();
